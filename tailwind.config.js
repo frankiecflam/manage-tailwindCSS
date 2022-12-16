420f2f23e8b0c993f8e903ce27c6b4e709057c14
Frankie Lam <frankie191193@gmail.com>
2022-12-16 10:32:42 +0000

    add additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
       fontFamily: {
         sans: ["Be Vietnam Pro", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        darkBlue: {
+          default: "hsl(228, 39%, 23%)",
+          100: "hsl(233, 12%, 13%)",
+        },
+        grayBlue: "hsl(227, 12%, 61%)",
+        paleRed: "hsl(13, 100%, 96%)",
+        lightGray: "hsl(0, 0%, 98%)",
+      },
     },
   },
   plugins: [],
