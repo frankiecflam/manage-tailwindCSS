d408660caffa8689cac0756d3181485c7e925d05
Frankie Lam <frankie191193@gmail.com>
2022-12-16 13:09:00 +0000

    set header to absolute bg-transparent

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,7 @@ import HeaderNav from "./HeaderNav";
 
 const Header = () => {
   return (
-    <header className="py-5 px-8">
+    <header className="py-5 px-8 absolute top-0 left-0 w-full h-auto z-10 bg-transparent">
       <HeaderNav />
     </header>
   );
--- src/components/header/HeaderNavList.tsx
+++ src/components/header/HeaderNavList.tsx
@@ -5,7 +5,7 @@ const HeaderNavList = ({ showMobileNav }: { showMobileNav: boolean }) => {
     <ul
       className={`${
         showMobileNav ? "flex" : "hidden"
-      } flex-1 justify-between items-center gap-x-10 absolute top-20 left-0 w-screen h-1/2 bg-lightGray flex-col py-5 font-medium text-xl tablet:flex tablet:relative tablet:w-auto tablet:h-auto tablet:top-0 tablet:flex-row tablet:justify-center tablet:mt-0 tablet:bg-transparent tablet:py-0 tablet:font-normal tablet:text-base`}
+      } flex-1 justify-between items-center gap-x-10 fixed top-20 left-0 w-screen h-1/2 bg-lightGray flex-col py-5 font-medium text-xl tablet:flex tablet:relative tablet:w-auto tablet:h-auto tablet:top-0 tablet:flex-row tablet:justify-center tablet:mt-0 tablet:bg-transparent tablet:py-0 tablet:font-normal tablet:text-base`}
     >
       <HeaderNavItem href="/pricing">pricing</HeaderNavItem>
       <HeaderNavItem href="/products">products</HeaderNavItem>
