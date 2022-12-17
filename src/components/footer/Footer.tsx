b0ca173fd3073fc28cec30ef4da7c85f3921e03f
Frankie Lam <frankie191193@gmail.com>
2022-12-17 10:41:27 +0000

    Add FooterNav

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,7 +4,7 @@ import FooterSubscribe from "./FooterSubscribe";
 
 const Footer = () => {
   return (
-    <footer className="px-8 py-12 bg-darkBlue-100 grid justify-center desktop:grid-cols-12 desktop:justify-between">
+    <footer className="px-8 py-12 bg-darkBlue-100 grid justify-center gap-y-10 desktop:grid-cols-12 desktop:justify-between">
       <FooterSubscribe />
       <FooterNav />
       <FooterSocial />
--- src/components/footer/FooterNav.tsx
+++ src/components/footer/FooterNav.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export function FooterNavItem({
+  children,
+  href,
+}: {
+  children: ReactNode;
+  href: string;
+}) {
+  return (
+    <li>
+      <Link href={href}>{children}</Link>
+    </li>
+  );
+}
 
 const FooterNav = () => {
-  return <div>FooterNav</div>;
+  return (
+    <nav className="text-white col-span-4">
+      <ul className="grid grid-cols-2 justify-between gap-y-3">
+        <FooterNavItem href="/">Home</FooterNavItem>
+        <FooterNavItem href="/careers">Careers</FooterNavItem>
+        <FooterNavItem href="/pricing">Pricing</FooterNavItem>
+        <FooterNavItem href="/Community">Community</FooterNavItem>
+        <FooterNavItem href="/Products">Products</FooterNavItem>
+        <FooterNavItem href="/privacy">Privacy Policy</FooterNavItem>
+        <FooterNavItem href="/about">About Us</FooterNavItem>
+      </ul>
+    </nav>
+  );
 };
 
 export default FooterNav;
